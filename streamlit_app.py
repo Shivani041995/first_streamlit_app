5d9a27501aa34caa18ca13a85b3bb596fc6ff001
Shivani Mahadik <110099312+Shivani041995@users.noreply.github.com>
2022-07-27 17:19:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
-streamlit.header('Breakfast Menu)
+streamlit.header('Breakfast Menu')
 streamlit.text('Sandwich')
                  streamlit.text('Kale,Spinach,Smoothie')
                  
